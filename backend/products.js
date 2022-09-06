2a035f7ce1114a325266635c0aaf3f52102b3806
aakankshabadame <aakankshabadame@gmail.com>
2022-09-06 14:18:53 +0530

    first commit

---
+++ backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "Sweet Corn Pizza",
+    brand: "dominos",
+    desc: "Cheese I Golden Corn | Cheese n Corn Pizza",
+    price: 300,
+    image:
+      "https://res.cloudinary.com/dyyyibla5/image/upload/v1660109142/food/hawaiian-pizza-with-ham-and-pineapple-picture-id1349383878_pknc7n.jpg",
+  },
+  {
+    id: 2,
+    name: "Pepper Barbecue Chicken",
+    brand: "Dominos",
+    desc: "Pepper barbecue chicken for that extra zing",
+    price: 200,
+    image:
+      "https://res.cloudinary.com/dyyyibla5/image/upload/v1660109112/food/pizza-and-hamburger-on-wooden-background-picture-id618546412_ddgr4y.jpg",
+  },
+  {
+    id: 3,
+    name: "veg classic burger",
+    brand: "Burger King",
+    desc: "BK Classic Veg + BK Classic Veg",
+    price: 198,
+    image:
+      "https://res.cloudinary.com/dyyyibla5/image/upload/v1660109082/food/bacon-and-cheese-burger-picture-id1323075103_z1csvc.jpg",
+  },
+];
+
+module.exports = products;
